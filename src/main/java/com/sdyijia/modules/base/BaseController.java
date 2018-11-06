876e403f2c27843805ef5bd640e257192d61dc81
shizhen <>
2018-11-06 11:20:24 +0800

    添加 BaseController.java 的 2个方法 重定向的方法 添加url问题

--- src/main/java/com/sdyijia/modules/base/BaseController.java
+++ src/main/java/com/sdyijia/modules/base/BaseController.java
@@ -1,9 +1,55 @@
 package com.sdyijia.modules.base;
 
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
 public abstract class BaseController {
 
-    public  static final  String REDIRECT = "redirect:";
-    public  static final  String FORWARD = "forward:";
+    public static final String REDIRECT = "redirect:";
+    public static final String FORWARD = "forward:";
 
+    public ModelAndView redirectUrl(Model m, String url) {
+        Map<String, Object> map = m.asMap();
+        ModelAndView mv = new ModelAndView();
+        RedirectView redirectView = new RedirectView(url);
+        mv.setView(redirectView);
+        mv.addAllObjects(map);
+        return mv;
+    }
+
+    public ModelAndView redirectUrl(String url) {
+        ModelAndView mv = new ModelAndView();
+        RedirectView redirectView = new RedirectView(url);
+        mv.setView(redirectView);
+        return mv;
+    }
+
+    /**
+     * 利用java反射机制将子类定义的常量放到model中
+     *
+     * @param c 子类的class
+     * @param m Model参数
+     */
+    public void addUrl(Class<? extends BaseController> c, Model m) {
+        Field[] fields = c.getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            Class<?> type = fields[i].getType();
+            if (type.equals(String.class)) {
+                String name = fields[i].getName();
+                if (!("POJO_NAME".equals(name) || "PERMISSION_PREFIX".equals(name) || "PERMISSION_URL_PREFIX".equals(name))) {
+                    System.out.println(fields[i].getName());
+                    try {
+                        m.addAttribute(fields[i].getName(), fields[i].get(c));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
 }
